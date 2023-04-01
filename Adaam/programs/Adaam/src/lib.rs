88248909172284c337b8fd01b1b16830bfd9545c
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-04-01 18:51:40 +0530

    add Init user wallet connect in contract

--- Adaam/programs/Adaam/src/lib.rs
+++ Adaam/programs/Adaam/src/lib.rs
@@ -1,3 +1,18 @@
 use anchor_lang::prelude::*;
 
+pub mod constant;
+pub mod state;
+use crate::{constant::*, state::*};
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod adamm {
+
+    use super::*;
+
+    pub fn init_user(ctx: Context<InitUser>,name:String,profile::String) --> Result<()>  {   // init user wallet address 
+
+        
+    } 
+}
